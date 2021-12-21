66f3361aa182a1840b4b3609bc3d0c3f7debad36
961026bm <olubunmiosunde.student@careerdevs.com>
2021-12-21 18:37:49 -0500

    FirstChallenge commit

--- src/com/careerdevs/Main.java
+++ src/com/careerdevs/Main.java
@@ -3,6 +3,11 @@ package com.careerdevs;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello Careerdevs");
+//    getFirstValue
+        System.out.println(getFirstValue(new int[]{1, 3, 5, 7, 9}));
+    }
+    // getFirstValue
+    public static int getFirstValue(int[] arr) {
+        return arr[0];
     }
 }
