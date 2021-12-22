8202a94d6f30d5dc431ccceade4de14d0300c45a
961026bm <olubunmiosunde.student@careerdevs.com>
2021-12-21 19:08:17 -0500

    SecondChallenge commit

--- src/com/careerdevs/Main.java
+++ src/com/careerdevs/Main.java
@@ -5,9 +5,16 @@ public class Main {
     public static void main(String[] args) {
 //    getFirstValue
         System.out.println(getFirstValue(new int[]{1, 3, 5, 7, 9}));
+        System.out.println(addition(123));
     }
     // getFirstValue
     public static int getFirstValue(int[] arr) {
         return arr[0];
     }
+    //    return next number in java
+    public static int addition(int num) {
+		int nextNum = num + 1;
+        return nextNum;
+    }
 }
+
